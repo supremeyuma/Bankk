eb88ad8469dee744867da00c714e5483a9dccedf
supremeyuma <37828971+supremeyuma@users.noreply.github.com>
2025-04-25 02:36:42 +0100

    ADDED PIN COLUMN TO USER TABLE

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TransferController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
